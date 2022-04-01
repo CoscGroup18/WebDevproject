6337312cbae6937a9876629561862fc782656ca8
pavithrard <rdilushripavithra1998@gmail.com>
2022-04-01 09:04:13 +0530

    new

--- societies.php
+++ societies.php
@@ -19,10 +19,34 @@
     <a href="#youth"><i 
       c0.lass="fas fa-arrow-up"></i></a>
   </div>
-  <section class="about" id="about">
+  <nav>
+    <div class="navbar">
+      <div class="logo"><a href="#">Socities</a></div>
+      <ul class="menu">
+        <li><a href="http://webenlance.com">Home</a></li>
+        <li><a href="http://webenlance.com">History</a></li>
+        <li><a href="http://webenlance.com">Societies</a></li>
+        <li><a href="http://webenlance.com">Sunday School</a></li>
+        <li><a href="http://webenlance.com">Holy Mass</a></li>
+        <li><a href="http://webenlance.com">Calender</a></li>
+        <li><a href="http://webenlance.com">Contact Us</a></li>
+          <div class="cancel-btn">
+            <i class="fas fa-times"></i>
+          </div>
+      </ul>
+      
+    </div>
+    <div class="menu-btn">
+      <i class="fas fa-bars"></i>
+    </div>
+  </nav>
+  
+
+  <section class="youth" id="youth">
     <div class="content">
       <div class="title"><span>Society of Youth</span></div>
-    <div class="about-details">
+    <div class="youth-details">
+    
       
       <div class="right">
         
@@ -45,6 +69,78 @@
     </div>
     </div>
   </section>
+
+  <section class="svp" id="svp">
+    <div class="content">
+      <div class="title"><span>St Vincent de Paul Society</span></div>
+    <div class="svp-details">
+    
+      
+      <div class="right">
+        
+        <p>
+          Society of was founded by the then Parish Priest late Rev. Fr. Nimal Ponweera on 16th November 1987, and since then our society has been actively working for 34 years.<br><br>
+
+          We all have the absolute conviction that the little services we do for the families in the Angulana community are so great before God.<br><br>
+          
+          With the unconditional support of a few potential members of our Angulana community, we help the families of our society living in Angulana, Ratmalana, Lakshapathiya and Kaldemulla areas as per the instructions of our Parish Priest.<br><br>
+          Special mention should be made of giving the Sunday school children a cup of milk tea with biscuits in the morning, and the supply of tea and food during the Shramadana at the church.<br><br>
+          We were also fortunate to provide financial assistance for the construction of the new church.
+          The general objective of our society is to grow further from the current number of 10 members, and to provide relief and comfort to all people of our community who are in need through spiritual and physical precepts.
+          At last but not least we ask for the blessings of God on all those who support us.
+          
+        </p>
+        <div class="button">
+          
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <section class="lgm" id="lgm">
+    <div class="content">
+      <div class="title"><span>The Legion of Mary</span></div>
+    <div class="lgm-details">
+    
+      
+      <div class="right">
+        
+        <p>
+          The Legion of Mary associated to St. Francis Xavier’s Church – Angulana was started in 7th October 1970 – on the day of the feast of Our Lady of Rosary.<br><br>
+Their main focus is to provide the physical and very specially the spiritual support. To provide this they visit families and make them meet the Priest if they are in need, guide the children who have gone astray to the Sunday school, help the Rev. Sisters to take the Holy Communion to the sick, arrange prayer services at sick peoples’ houses.  
+
+        </p>
+        <div class="button">
+          
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
+  <section class="sh" id="sh">
+    <div class="content">
+      <div class="title"><span>The Sacred Heart Captol</span></div>
+    <div class="lgm-details">
+    
+      
+      <div class="right">
+        
+        <p>
+          The Society of the Sacred Heart of our church was established by late Rev. Fr. Edward Ravelpulle on 23rd June 1995. The purpose for establishing this society was to nourish the souls of the faithful of the church. In the beginning the president was Mrs. Eda Fonseka, Vice President was Mrs. Tethla Peiris, Secretary was Mrs. Catherine Fernando, and Treasurer was Mrs. Senaratne.<br><br>
+          To accomplish the purpose of establishment which is to uplift the spiritual life of the faithful, the society gather on the first Fridays of every month and chant the Sacred Hour. Then along with the Priest, they take part in the Eucharistic Adoration and Benediction and then the Holy Mass. When the Catholics around the globe were preparing for the commemoration of the 2nd Millennium of the Birth of Christ, to take part in this commemoration as a Parish Community, along with the then Parish Priest Rev. Fr. Edward Ravelpulle the members of our society when through houses of the different zones of our church and consecrated many the families to the Sacred Heart of Jesus.<br><br>
+           Not only in the spiritual work, the members of the society contribute to all other works in the church, both financially and from work.<br><br>
+          
+        </p>
+        <div class="button">
+          
+        </div>
+      </div>
+    </div>
+    </div>
+  </section>
+
 
   <footer class="footer">
     <div class="container bottom_border" style="margin-left: 120px;">
